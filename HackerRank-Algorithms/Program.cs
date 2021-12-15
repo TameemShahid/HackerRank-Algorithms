0d217d12056d52eceb24d843ccbe5b4795552577
Tameem Shahid <muhammad.tameemshahid@kineticsoftware.com>
2021-12-15 15:25:37 +0500

    Add project files.

---
+++ HackerRank-Algorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
